e037ab55b8360b1e09c9dad966c3086044c29361
kris-ro <kris_ro@yahoo.com>
2026-06-01 18:46:38 +0300

    excluded GET params from url/components

--- src/Config.php
+++ src/Config.php
@@ -118,7 +118,7 @@ class Config {
       'domain' => $_SERVER['SERVER_NAME'],
       'uri' => $_SERVER['REQUEST_URI'],
       'query_string' => $_SERVER['QUERY_STRING'],
-      'components' => preg_split('%(/|\?|&)%', trim($_SERVER['REQUEST_URI'], '/')),
+      'components' => preg_split('%(/)%', trim(current(explode('?', $_SERVER['REQUEST_URI'])), '/')),
     ] : [
       'protocol' => '',
       'domain' => '',
